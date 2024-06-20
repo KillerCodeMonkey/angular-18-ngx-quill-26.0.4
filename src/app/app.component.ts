27cf57b9bc8c3c03b10d580e5f1f93d64bad8349
Bengt Weiße <bengtler@gmail.com>
2024-06-20 19:30:48 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { QuillEditorComponent } from 'ngx-quill';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QuillEditorComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'empty-angular';
+  content = 'editor'
 }
